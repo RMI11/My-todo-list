99b1c15c5c760a1d61efbeded1cee9ef4386e830
Rajko Radojkovic <radojkovic2020@gmail.com>
2022-09-08 14:06:32 +0200

    My todo list

---
+++ Script.js
@@ -0,0 +1,34 @@
+function Add() {
+    let novaStavka = document.querySelector("#input");
+    novaStavka = novaStavka.value;
+    let stavka = document.createElement("li");
+    stavka.className = "stavka";
+    stavka.innerHTML = `${novaStavka}`;
+
+    document.querySelector("ul").appendChild(stavka);
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+var span = document.createElement("SPAN");
+var txt = document.createTextNode("x");
+span.className = "close";
+span.appendChild(txt);
+myNodelist[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+close[i].onclick = function() {
+var div = this.parentElement;
+div.style.display = "none";
+}}
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
